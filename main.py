5789d6ab8f1f0c235fc7a4a7d674913a71d1f0db
adminw <vip198926@vip.qq.com>
2021-01-30 21:48:20 +0800

    修改开始任务参数，减少间隔时间

--- main.py
+++ main.py
@@ -13,4 +13,9 @@ from wxSys import brushAds
 
 if __name__ == '__main__':
     start_tool = brushAds()
-    start_tool.missionStart()
+    # 开始任务
+    # sub_num – 提交次数，默认20次
+    # interval – 请求后与提交之间的间隔时间，默认30秒
+    # retryInterval – 重试提交或第二次提交间隔时间，默认10秒
+    # retry – 提交返回空后重试次数，默认3次
+    start_tool.missionStart(sub_num=20, interval=20, retryInterval=5, retry=2)
