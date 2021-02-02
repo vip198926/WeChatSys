3efd0fc532ec32632d25dfdfd3dd802f8fb2fa63
adminw <vip198926@vip.qq.com>
2021-02-03 00:35:37 +0800

    重写获取配置文件

--- config.py
+++ config.py
@@ -1,36 +1,28 @@
-#!/usr/bin/env python
-# -*- encoding=utf8 -*-
-
-import os
+# -*- coding: utf-8 -*-
 import configparser
-
-
-class EnvInterpolation(configparser.BasicInterpolation):
-    """
-    尝试从系统环境变量中读取，如果读取失败，则直接使用配置值
-    """
-
-    def before_get(self, parser, section, option, value, defaults):
-        value = super().before_get(parser, section, option, value, defaults)
-        # 无法获取则返回原值
-        return os.path.expandvars(value)
+import os
 
 
 class Config(object):
+
     def __init__(self, config_file='config.ini'):
         self._path = os.path.join(os.getcwd(), config_file)
         if not os.path.exists(self._path):
             raise FileNotFoundError("No such file: config.ini")
-        self._config = configparser.ConfigParser(interpolation=EnvInterpolation())
-        self._config.read(self._path, encoding='utf-8-sig')
-        self._configRaw = configparser.RawConfigParser(interpolation=EnvInterpolation())
-        self._configRaw.read(self._path, encoding='utf-8-sig')
+        self._config = configparser.ConfigParser()
+        self._config.read(self._path, encoding='utf-8')
 
-    def get(self, section, name):
-        return self._config.get(section, name)
+    def get(self, section, name, strip_blank=True, strip_quote=True):
+        s = self._config.get(section, name)
+        if strip_blank:
+            s = s.strip()
+        if strip_quote:
+            s = s.strip('"').strip("'")
 
-    def getRaw(self, section, name):
-        return self._configRaw.get(section, name)
+        return s
+
+    def getboolean(self, section, name):
+        return self._config.getboolean(section, name)
 
 
 global_config = Config()
