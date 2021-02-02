5dfbcfa084a0d29718b78970eafeaa0b3576e7ec
adminw <vip198926@vip.qq.com>
2021-02-03 00:33:15 +0800

    从配置文件中读取日志级别并设置日志级别

--- logger.py
+++ logger.py
@@ -10,6 +10,8 @@ import logging.handlers
 import os
 import time
 
+from config import global_config
+
 '''
 日志模块
 '''
@@ -23,11 +25,21 @@ if not os.path.exists(logs_dir):
 
 LOG_FILENAME = logs_name
 logger = logging.getLogger()
+LOG_LEVEL = global_config.get('config', 'log_level')
+
+
+# 从配置文件中读取日志级别并设置日志级别
+if LOG_LEVEL == 'debug':
+    log_level = logging.DEBUG
+elif LOG_LEVEL == 'info':
+    log_level = logging.INFO
+elif LOG_LEVEL == 'warn':
+    log_level = logging.WARNING
 
 
 def set_logger():
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+    logger.setLevel(log_level)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s', datefmt='%m-%d %H:%M:%S')
     # formatter = logging.Formatter('%(asctime)s - %(process)d-%(threadName)s - '
     #                               '%(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
     console_handler = logging.StreamHandler()
